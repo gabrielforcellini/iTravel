40f148f8dbcfa4574cf754ef100f272e498e5019
Gabriel <gabriel.forcellini@hotmail.com>
2023-05-18 20:07:26 -0300

    criando telas de dados

---
+++ app/src/main/java/com/unesc/itravel/Dados1Activity.java
@@ -0,0 +1,13 @@
+package com.unesc.itravel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dados1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_dados_1);
+    }
+}
---
+++ app/src/main/java/com/unesc/itravel/GasolinaActivity.java
@@ -0,0 +1,14 @@
+package com.unesc.itravel;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GasolinaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gasolina);
+    }
+}
