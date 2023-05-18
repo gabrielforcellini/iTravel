8aa202eae23e549ee3435da7beeaef9b9b219a6d
Gabriel <gabriel.forcellini@hotmail.com>
2023-05-18 19:10:06 -0300

    criado telas de login e de carregamento

---
+++ app/src/main/java/com/unesc/itravel/splashActivity.java
@@ -0,0 +1,17 @@
+package com.unesc.itravel;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashActivity extends AppCompatActivity {
+    ImageView my_image_view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash;
+
+        my_image_view =
+    }
+}
