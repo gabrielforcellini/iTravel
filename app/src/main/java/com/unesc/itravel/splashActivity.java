9b49813f068ff02ae9d519c372984144bdc87a9b
Gabriel <gabriel.forcellini@hotmail.com>
2023-05-18 19:26:32 -0300

    criado animacao

--- app/src/main/java/com/unesc/itravel/splashActivity.java
+++ app/src/main/java/com/unesc/itravel/splashActivity.java
@@ -1,17 +1,43 @@
 package com.unesc.itravel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splashActivity extends AppCompatActivity {
-    ImageView my_image_view;
+    private ImageView my_image_view;
+
+    private Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash;
+        setContentView(R.layout.activity_splash);
 
-        my_image_view =
+        my_image_view = findViewById(R.id.my_image_view);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.splash);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(splashActivity.this, LoginActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        my_image_view.setAnimation(animation);
+        animation.start();
     }
 }
