c0820707c38e9552db567433283c6242a4a69e91
Luana Nietto <niettoluana@gmail.com>
2023-05-31 22:47:09 -0300

    Adicionado um botão de voltar para tela inicial em Criar conta; adicionado a flag para a senha ficar oculta nos inputs de senha.

--- app/src/main/java/com/unesc/itravel/CriaContaActivity.java
+++ app/src/main/java/com/unesc/itravel/CriaContaActivity.java
@@ -19,6 +19,7 @@ public class CriaContaActivity extends AppCompatActivity {
     private EditText edt_senha;
     private EditText edt_confirma_senha;
     private Button btn_salvar;
+    private Button btn_voltar;
     private LoginDAO loginDAO;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +31,7 @@ public class CriaContaActivity extends AppCompatActivity {
         edt_senha = findViewById(R.id.edt_senha);
         edt_confirma_senha = findViewById(R.id.edt_confirma_senha);
         btn_salvar = findViewById(R.id.btn_salvar);
+        btn_voltar = findViewById(R.id.btn_voltar);
 
         btn_salvar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +51,12 @@ public class CriaContaActivity extends AppCompatActivity {
                 Login login = new Login(nome, usuario, senha);
                 loginDAO.insert(login);
                 Snackbar.make(findViewById(android.R.id.content), "Usuário criado.", Snackbar.LENGTH_SHORT).show();
+                startActivity(new Intent(CriaContaActivity.this, Dados1Activity.class));
+            }
+        });
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 startActivity(new Intent(CriaContaActivity.this, LoginActivity.class));
             }
         });
